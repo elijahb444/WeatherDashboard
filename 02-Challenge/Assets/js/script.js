7cc5e35e217ef12240062e76f8e6c8f4b91d5bde
Elijah Brown <elijahab716@gmail.com>
2024-06-07 16:07:56 -0500

    configured geocode and forecast apis

--- 02-Challenge/Assets/js/script.js
+++ 02-Challenge/Assets/js/script.js
@@ -8,44 +8,32 @@ const apiKey = 'a3aae8933b04e64f40888cfe629f2e70'
 
 //const forecastUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
 
-const getCoords = function() {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userSearch.value},&limit=5&appid=${apiKey}`)
-    .then(function (response) {
+const getForecast = function() {
+    const getCoords = function() {
+        return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userSearch.value}&limit=5&appid=${apiKey}`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                const cityLat = data[0].lat;
+                const cityLon = data[0].lon;
+                console.log(`Latitude: ${cityLat}, Longitude: ${cityLon}`);
+                return { lat: cityLat, lon: cityLon };
+            });
+    };
+
+    getCoords().then(function(coords) {
+        const { lat, lon } = coords;
+        return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+    })
+    .then(function(response) {
         return response.json();
     })
-    .then(function (data) {
-        console.log(data)
-        const cityLat = data[0].lat
-        const cityLon = data[0].lon
-        // targets latitude and longitude of top result
-        console.log(`Latitude: ${cityLat}, Longitude: ${cityLon}`)
-        return {cityLat, cityLon}
-       })
-}
+    .then(function(data) {
+        console.log(data);
+    });
+};
 
-const getForecast = function () {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userSearch.value},&limit=5&appid=${apiKey}`)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-        const cityLat = data[0].lat
-        const cityLon = data[0].lon
-        // targets latitude and longitude of top result
-        console.log(`Latitude: ${cityLat}, Longitude: ${cityLon}`)
-        return {cityLat, cityLon}
-       })
-
-
-
-    fetch(`api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        })
-}
+// TODO: find a way to parse/iterate through forecast results
 
 searchBtn.addEventListener('click', getForecast)
